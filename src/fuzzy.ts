fcb14779af21a39d2fb89f926365df3848e8284f
greenpizza1203 <konakarthik12@gmail.com>
2020-12-25 13:16:56 -0500

    changes

--- src/fuzzy.ts
+++ src/fuzzy.ts
@@ -1,26 +1,53 @@
-import fuzzysort from "fuzzysort";
+import Fuse from "fuse.js";
 
 const db: { [key: string]: set } = {}
-let prepared: card[]
-let options = {keys: ["wordPrepared", "definitionPrepared"]};
+let fuse: Fuse<card>
+// let options = {keys: ["wordPrepared", "definitionPrepared"]};
+const options = {
+    // isCaseSensitive: false,
+    includeScore: true,
+    // shouldSort: true,
+    // includeMatches: false,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    // threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    keys: [
+        "word",
+        "definition"
+    ]
+};
+
+
+// Change the pattern
+const pattern = ""
 
 export function prepareSearch(sets: any) {
     Object.assign(db, sets)
     let targets = Object.values(db).flatMap(set => set)
-    prepared = targets.map(t => ({
-        ...t,
-        wordPrepared: fuzzysort.prepare(t.word),
-        definitionPrepared: fuzzysort.prepare(t.definition)
-    }))
+    fuse = new Fuse(targets, options);
 
-    console.log(prepared)
+    // prepared = targets.map(t => ({
+    //     ...t,
+    //     wordPrepared: fuzzysort.prepare(t.word),
+    //     definitionPrepared: fuzzysort.prepare(t.definition)
+    // }))
+    //
+    // console.log(prepared)
 
 
 }
 
 export function searchFuzzy(question: string) {
-    if (!prepared) return
-    const results = fuzzysort.go(question, prepared, options);
-    return results[0]?.obj
+    // if (!prepared) return
+    // const results = fuzzysort.go(question, prepared, options);
+    // return results[0]?.obj
+    let search = fuse.search(question);
+    console.log(search)
+    return search
 
 }
